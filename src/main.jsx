c83bf63366c5bab21c3678209a92c4639b809cd2
rc3689 <ronishchaudhary0228@gmail.com>
2025-08-28 08:38:09 +0545

    api improved

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { AuthContext } from "./context/AuthContext";
+
+import { AuthProvider } from "./context/AuthContext";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <AuthContext>
+    <AuthProvider>
       <App />
-    </AuthContext>
+    </AuthProvider>
   </StrictMode>
 );
